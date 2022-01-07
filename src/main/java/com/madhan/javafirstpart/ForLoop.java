75e8c632090eeb35a2cba03ef84b0d9ce78db2d5
Madhankumar Selvaraj <Madhankumar Selvaraj@chakranetwork.com>
2022-01-07 23:22:46 +0530

    Security Implementation

---
+++ src/main/java/com/madhan/javafirstpart/ForLoop.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.madhan.javafirstpart;
+
+/**
+ *
+ * @author Madhankumar Selvaraj
+ */
+public class ForLoop {
+
+public static void main(String[] args) {
+
+    
+    // Here I is initial value 0
+    // This going to increament the value to 9
+    // This going to increament the value to 9
+    for (int i = 0; i < 10; i++) {
+        System.out.println("" + i);
+    }
+
+}
+
+}
---
+++ src/main/java/com/madhan/javafirstpart/WhileLoop.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.madhan.javafirstpart;
+
+/**
+ *
+ * @author Madhankumar Selvaraj
+ */
+public class WhileLoop {
+
+public static void main(String[] args) {
+    //This is loop concept
+    int i = 0; // This is initialize the zero.
+
+    while (i < 100) { // This is condition for check the value to less then 100
+
+        i++; // this is increament the value to 100.
+        System.out.println("Value : " + i);
+
+    }
+
+}
+
+}
